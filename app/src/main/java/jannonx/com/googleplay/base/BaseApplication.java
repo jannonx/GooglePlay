33cbd9bed839ceedd57ce41fc039e7bf2a5284ec
jannonx <jannonx@foxmail.com>
2016-12-01 16:54:51 +0800

    创建BaseApplication

---
+++ app/src/main/java/jannonx/com/googleplay/base/BaseApplication.java
@@ -0,0 +1,33 @@
+package jannonx.com.googleplay.base;
+
+import android.app.Application;
+import android.content.Context;
+import android.os.Handler;
+import android.os.Process;
+
+/**
+ * @项目名 GooglePlay
+ * @创建者 jannonx
+ * @创建时间 2016/12/1-下午4:43
+ * @描述信息 desc
+ */
+
+public class BaseApplication extends Application {
+
+    private static long mMainThread;
+    private static Handler mHandler;
+    private static Context mContext;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //上下文
+        mContext = getApplicationContext();
+
+        //放置一个主线程的Handler
+        mHandler = new Handler();
+
+        //得到主线的id
+        mMainThread = Process.myTid();
+    }
+}
